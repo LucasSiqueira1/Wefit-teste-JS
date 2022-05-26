036dacf301ae9663c2fcef31d3bb98db07a1c2be
LucasSiqueira1 <Lucas_siqueira28@outlook.com>
2022-05-26 19:23:44 -0300

    feat: cards

--- wefit-test/js/main.js
+++ wefit-test/js/main.js
@@ -21,3 +21,23 @@ headerContainer.style.color = 'white';
 const buttonMore = headerContainer.getElementsByTagName("a")[0];
 buttonMore.style.backgroundColor = '#27a844';
 buttonMore.style.border = 'none';
+
+
+// Cards
+const cardContent = Array.from(document.querySelectorAll(".card"));
+const cardTitle = Array.from(document.querySelectorAll(".card-title"));
+
+cardTitle.forEach((titleCard) => {
+  const cardNext = titleCard.closest("div.card");
+  cardNext.dataset['title'] = titleCard.textContent;
+});
+
+const cardAnimalTitle = document.querySelector('[data-title="Animais"] .btn');
+cardAnimalTitle.classList.remove("btn-primary");
+cardAnimalTitle.classList.add("btn-success");
+
+const cardBefore = cardContent[0].offsetParent.parentElement;
+cardBefore.insertBefore(cardContent[3].offsetParent, cardContent[0].offsetParent);
+
+const cardAfter = cardContent[1].offsetParent.parentElement;
+cardAfter.insertBefore(cardContent[2].offsetParent, cardContent[0].offsetParent.nextSibling);
